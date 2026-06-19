3f87364b108d09d2f3f6a934f41baf406d904f6d
sandeep2409-gitch <sandeepmanchinasetti007@gmail.com>
2026-06-19 11:28:46 +0530

    chore: resolve dotenv path absolutely in server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 app.use(cors());
